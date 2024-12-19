473d7c3b1fefa81555d468bf475fb32fde64945a
hdx <121@localhost.com>
2024-12-19 10:17:06 +0800

    @Update 1. 添加字体和dmp导出

--- src/main/src/Main.cpp
+++ src/main/src/Main.cpp
@@ -1,14 +1,90 @@
-#include "mainwindow.h"
-
 #include <QApplication>
+#include <QFontDatabase>
 
-int main(int argc, char *argv[])
-{
+#include "Mainwindow.h"
+
+#ifdef WIN32
+// clang-format off
+#include <Windows.h>
+#include <DbgHelp.h>
+#pragma comment(lib, "DbgHelp.lib")
+// clang-format on
+LONG ApplicationCrashHandler(EXCEPTION_POINTERS* pException);
+
+#endif
+
+
+int main(int argc, char* argv[]) {
+#ifdef WIN32
+    SetUnhandledExceptionFilter((LPTOP_LEVEL_EXCEPTION_FILTER)ApplicationCrashHandler);
+#endif
+
     QApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
     QApplication a(argc, argv);
+
+    int fontId = QFontDatabase::addApplicationFont(":/font/WenQuanWeiMiHei.ttf");
+    QString mysh = QFontDatabase::applicationFontFamilies(fontId).at(0);
+    QFont font(mysh, 10);
+    QApplication::setFont(font);
+
     MainWindow w;
     w.show();
     return a.exec();
 }
+
+
+// Unhandled Exception的回调函数
+#ifdef WIN32
+
+#include <string>
+#include <sstream>
+#include <iomanip>
+
+// 创建Dump文件
+void CreateDumpFile(LPCWSTR lpstrDumpFilePathName, EXCEPTION_POINTERS* pException) {
+    CreateDirectory("./dump", NULL);
+    HANDLE hDumpFile =
+        CreateFileW(lpstrDumpFilePathName, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+    // Dump信息
+    MINIDUMP_EXCEPTION_INFORMATION dumpInfo;
+    dumpInfo.ExceptionPointers = pException;
+    dumpInfo.ThreadId = GetCurrentThreadId();
+    dumpInfo.ClientPointers = TRUE;
+    // 写入Dump文件内容
+    MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(), hDumpFile, MiniDumpNormal, &dumpInfo, NULL, NULL);
+    CloseHandle(hDumpFile);
+}
+
+std::wstring String2Wstring(std::string wstr) {
+    std::wstring res;
+    int len = MultiByteToWideChar(CP_ACP, 0, wstr.c_str(), wstr.size(), nullptr, 0);
+    if (len < 0) {
+        return res;
+    }
+    wchar_t* buffer = new wchar_t[len + 1];
+    if (buffer == nullptr) {
+        return res;
+    }
+    MultiByteToWideChar(CP_ACP, 0, wstr.c_str(), wstr.size(), buffer, len);
+    buffer[len] = '\0';
+    res.append(buffer);
+    delete[] buffer;
+    return res;
+}
+
+// 处理Unhandled Exception的回调函数
+LONG ApplicationCrashHandler(EXCEPTION_POINTERS* pException) {
+    auto t = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    std::stringstream ss;
+    ss << std::put_time(std::localtime(&t), "%Y%m%d_%H%M%S");
+    std::string file_name = ss.str();
+    std::string time_str = file_name;
+    std::wstring dump_file = String2Wstring("./dump/" + time_str + ".dmp");
+
+    CreateDumpFile(dump_file.c_str(), pException);
+    return EXCEPTION_EXECUTE_HANDLER;
+}
+
+#endif
