7656b2f5fca5c758140fb9a48e3286acb141feb9
hdx <121@localhost.com>
2024-12-19 10:10:17 +0800

    @Update 1. 优化代码架构

---
+++ script/qt_setup.cmake
@@ -0,0 +1,15 @@
+# Qt配置
+set(CMAKE_AUTOUIC ON)
+set(CMAKE_AUTOMOC ON)
+set(CMAKE_AUTORCC ON)
+
+set(QT_VERSION_MAJOR 5)
+set(QT_COMPONENTS Widgets Core Gui)
+set(QT_LIBRARIES "" CACHE INTERNAL "QtLibraries")
+
+foreach(item ${QT_COMPONENTS})
+    list(APPEND QT_LIBRARIES Qt${QT_VERSION_MAJOR}::${item})
+    find_package(Qt${QT_VERSION_MAJOR} REQUIRED COMPONENTS ${item})
+endforeach()
+
+message(STATUS ${QT_LIBRARIES})
--- src/main/src/mainwindow.cpp
+++ src/main/src/mainwindow.cpp
@@ -1,5 +1,5 @@
-#include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "Mainwindow.h"
+#include "./ui_Mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
